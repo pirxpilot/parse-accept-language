8c0920cf331023a0461dfe903cdcbd78a87574fe
Damian Krzeminski <pirxpilot@code42day.com>
2015-05-08 20:49:15 -0400

    Initial commit

---
+++ test/parse-accept-language.js
@@ -0,0 +1,9 @@
+var should = require('should');
+var parseAcceptLanguage = require('../');
+
+describe('parse-accept-language node module', function () {
+  it('must have at least one test', function () {
+    parseAcceptLanguage();
+    should.fail('Need to write tests.');
+  });
+});
