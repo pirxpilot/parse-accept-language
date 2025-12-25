41539fdbebeb93ece2624ecbf5c70d4e2de50012
Damian Krzeminski <pirxpilot@furkot.com>
2025-12-25 09:16:12 -0700

    upgrade `biome` to 2.3.10

--- test/parse-accept-language.js
+++ test/parse-accept-language.js
@@ -14,8 +14,8 @@ function pluck(arr, prop) {
   return arr.map(item => item[prop]);
 }
 
-describe('parse-accept-language node module', function () {
-  it('parse empty', function () {
+describe('parse-accept-language node module', () => {
+  it('parse empty', () => {
     let req = request('');
     let parsed = pal(req);
 
@@ -28,7 +28,7 @@ describe('parse-accept-language node module', function () {
     assert.equal(parsed.length, 0);
   });
 
-  it('parse standard', function () {
+  it('parse standard', () => {
     const req = request('en-US,en;q=0.8,pl;q=0.6');
     const parsed = pal(req);
 
@@ -55,14 +55,14 @@ describe('parse-accept-language node module', function () {
     assert.equal(parsed2, parsed);
   });
 
-  it('only parse once per request', function () {
+  it('only parse once per request', () => {
     const req = request('en-US,en;q=0.8,pl;q=0.6');
     const parsed1 = pal(req);
     const parsed2 = pal(req);
     assert.equal(parsed2, parsed1);
   });
 
-  it('parse out of order', function () {
+  it('parse out of order', () => {
     const req = request('de-DE;q=0.2,pl,en-GB;q=0.6');
     const parsed = pal(req);
     assert.ok(Array.isArray(parsed));
@@ -85,7 +85,7 @@ describe('parse-accept-language node module', function () {
     assert.equal(parsed[2].language, 'de');
   });
 
-  it('parse country variants', function () {
+  it('parse country variants', () => {
     const req = request('de-DE,de-AT;q=0.8,de;q=0.6,en-US;q=0.4,en;q=0.2');
     const parsed = pal(req);
 
@@ -98,7 +98,7 @@ describe('parse-accept-language node module', function () {
     assert.deepEqual(pluck(parsed, 'q'), [1, 0.8, 0.6, 0.4, 0.2]);
   });
 
-  it('convert region to uppercase', function () {
+  it('convert region to uppercase', () => {
     const req = request('en-us');
     const parsed = pal(req);
 
